0e443053aee7d1641b0993e8a732d89f593fa46f
Clapclap-tech <kenjieemmanuellagaras@gmail.com>
2025-03-24 22:18:36 +0800

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+import SignIn from './components/SignIn'
+
+function App() {
+    return (
+      <>
+          <SignIn/>
+      </>
+    );
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+
+
+function Header() {
+    return (
+        <header>
+            <h1 className="text-red-600">Attendance Checker</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Attendance</a></li>
+                    <li><a href="#">Profile</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header

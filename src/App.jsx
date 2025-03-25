c413756a0761c65b2b600e8ec36481cf5bfe09dc
Clapclap-tech <kenjieemmanuellagaras@gmail.com>
2025-03-25 21:11:47 +0800

    Authentication V0.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
 import SignIn from './components/SignIn'
+import SignUp from './components/SignUp'
 
 function App() {
     return (
       <>
-          <SignIn/>
+          <SignUp/>
       </>
     );
 }

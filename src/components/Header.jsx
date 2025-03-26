6cde6d58dde054450f2b94a78b8e42280ea9424c
Clapclap-tech <kenjieemmanuellagaras@gmail.com>
2025-03-26 16:17:56 +0800

    test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,7 @@
 
 function Header() {
     return (
-        <header>
-            <h1 className="text-red-600">Attendance Checker</h1>
-            <nav>
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Attendance</a></li>
-                    <li><a href="#">Profile</a></li>
-                </ul>
-            </nav>
-        </header>
+        <h1></h1>
     );
 }
 

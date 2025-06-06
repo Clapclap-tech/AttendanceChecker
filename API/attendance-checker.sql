a43b2115ade2913592ba269951129558492ea3a8
Clapclap-tech <kenjieemmanuellagaras@gmail.com>
2025-06-06 10:37:51 +0800

    attendance-checker sql

--- API/attendance-checker.sql
+++ API/attendance-checker.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 24, 2025 at 11:51 AM
+-- Generation Time: Jun 06, 2025 at 04:37 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,31 @@ SET time_zone = "+00:00";
 --
 -- Database: `attendance-checker`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `classes`
+--
+
+CREATE TABLE `classes` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `section` varchar(255) DEFAULT NULL,
+  `subject` varchar(255) DEFAULT NULL,
+  `room` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `classes`
+--
+
+INSERT INTO `classes` (`id`, `name`, `section`, `subject`, `room`, `created_at`) VALUES
+(1, 'asdasd', 'asda', 'das', 'adsdasd', '2025-06-06 01:08:31'),
+(2, 'asda', 'asdasda', 'asdas', 'asda', '2025-06-06 01:18:40'),
+(3, 'asd', 'asd', 'asdasd', 'asd', '2025-06-06 01:19:07'),
+(4, 'asd', 'asdasd', 'asdasd', 'asdas', '2025-06-06 01:25:00');
 
 -- --------------------------------------------------------
 
@@ -44,11 +69,37 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`studentID`, `email`, `password`, `firstName`, `lastName`, `birthdate`, `streetAddress`, `cityAddress`, `zipCode`) VALUES
+(3221316, 'kenjie090502@gmail.com', '$2y$10$kaA7xO8S/tpa7xhae5FeUu2cO/dR7Eg3DAV5mtYm0ZYsFVoCl1bt2', 'Kenjie', 'Lagaras', '2002-05-09', 'Blk.2 Lot.16(5713)(Red Plate) Villa Azalea phase2, Cotcot Li', 'Cebu', '6002'),
 (3221318, 'kenjielagaras1@gmail.com', '$2y$10$Wr9Waa1eh35r.', 'Kenjie', 'Lagaras', '2002-09-05', 'Blk.2 Lot.16(5713)(Red Plate) Villa Azalea phase2, Cotcot Li', 'Cebu', '6002'),
 (3221516, 'kenjielagaras@gmail.com', '$2y$10$gVJRc1J2.tb2f', 'kenjie', 'lagaras', '2002-05-09', 'Blk.2 Lot.16(5713)(Red Plate) Villa Azalea phase2, Cotcot Li', 'Cebu', '6002'),
 (3221758, 'klagaras14@gmail.com', '$2y$10$q2ZD0hv4qHxDJf4K.Ir1L.x1NiheS61aTmIapqzCfZ9W9uAwAdwpm', 'Kenjie', 'Lagaras', '2002-05-09', 'Blk.2 Lot.16(5713)(Red Plate) Villa Azalea phase2, Cotcot Li', 'Cebu', '6002'),
-(3221316, 'kenjie090502@gmail.com', '$2y$10$kaA7xO8S/tpa7xhae5FeUu2cO/dR7Eg3DAV5mtYm0ZYsFVoCl1bt2', 'Kenjie', 'Lagaras', '2002-05-09', 'Blk.2 Lot.16(5713)(Red Plate) Villa Azalea phase2, Cotcot Li', 'Cebu', '6002'),
 (12312312, 'kenjielagaras2@gmail.com', '$2y$10$CAWA0KB2G2fNAg9GqcOIL.Q3l63HapHZYLi1Al/E2wTNueyKFc7Cq', '1231231', '1231231', '2002-05-09', 'Blk.2 Lot.16(5713)(Red Plate) Villa Azalea phase2, Cotcot Li', 'Cebu', '6002');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `classes`
+--
+ALTER TABLE `classes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`studentID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `classes`
+--
+ALTER TABLE `classes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
